3b8eecdc85fa58a9ddaaeb3a935b53c30f990dce
sterling_hebert <sterling_hebert@outlook.com>
2022-07-20 18:42:51 -0500

    2nd

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -12,11 +12,27 @@ reverse("id"); // "di"
 reverse(""); // ""
 ***********************************************************************/
 
-
 function reverse(str) {
-  // Your code here
+  console.log(str)
+  if(str.length <= 1){
+    return str;
+  }
+  return reverse(str.substring(1)) + str.charAt(0);
 }
+reverse("house");
 
+// function reverse2(str) {
+//   if (str === null) {
+//     return null;
+//   }
+//   if (str.length <= 1) {
+//     return str;
+//   }
+//   var first = str[0];
+//   var last = str[str.length - 1];
+//   var str1 = reverse2(str.substring(1, str.length - 1));
+//   return last + str1 + first;
+// }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
