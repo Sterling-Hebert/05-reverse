30143930e01834ed74354903078cf9f6b5a18ece
sterling_hebert <sterling_hebert@outlook.com>
2022-07-20 19:15:02 -0500

    3rd commit

--- problems/06-add-to-twelve.js
+++ problems/06-add-to-twelve.js
@@ -5,19 +5,26 @@ return false.
 
 Examples:
 
-addToTwelve([1, 3, 4, 7, 5]); // true
-addToTwelve([1, 3, 4, 7, 6]); // false
-addToTwelve([1, 11, 4, 7, 6]); // true
-addToTwelve([1, 12, 4, 7, 6]); // false
-addToTwelve([1]); // false
+
 ***********************************************************************/
 
 
-function addToTwelve(arr) {
-
+function addToTwelve(arr, arr2 = arr.slice()) {
+  if (!arr2.length){
+    return false;
+  }
+  if(arr2[0] + arr2[1] === 12){
+    return true
+  }
+  console.log(arr2.slice(1))
+return addToTwelve(arr, arr2.slice(1))
 }
 
-
+console.log(addToTwelve([1, 3, 4, 7, 5])); // true
+console.log(addToTwelve([1, 3, 4, 7, 6])); // false
+console.log(addToTwelve([1, 11, 4, 7, 6])); // true
+console.log(addToTwelve([1, 12, 4, 7, 6])); // false
+console.log(addToTwelve([1])); // false
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = addToTwelve;
